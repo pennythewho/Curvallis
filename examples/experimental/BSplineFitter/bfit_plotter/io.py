da19c701f828e606c7de3699a5bbd1252715c479
pennythewho <pennythewho@gmail.com>
2020-09-28 15:21:56 -0700

    add functions to determine columns, initialize output, parse line

--- examples/experimental/BSplineFitter/bfit_plotter/io.py
+++ examples/experimental/BSplineFitter/bfit_plotter/io.py
@@ -1,7 +1,11 @@
 import csv
 from collections import namedtuple
 import numpy as np
+from bfit import fit
 
+xcol = 'x'
+ycol = 'y'
+wcol = 'w'
 dy_template = 'd{0}y'
 dyw_template = 'd{0}w'
 
@@ -34,7 +38,8 @@ def load_data(path, p, with_weights=[]):
                             as documented in bfit.fit.get_bspline_fit
     """
     # determine expected columns and initialize dictionary with expected keys
-
+    cols = _get_expected_cols(p, with_weights)
+    out = _initialize_output(cols)
     # is file present?
     # load file as csv
     #
@@ -45,7 +50,56 @@ def load_data(path, p, with_weights=[]):
 
 def _get_expected_cols(p, with_weights):
     """ returns an ordered list of columns in the data file """
-    cols = ['x', 'y'] + (['w'] if 0 in with_weights else [])
+    cols = [xcol, ycol]
+    if 0 in with_weights:
+        cols = cols + [wcol]
     for d in range(1, p+1):
         cols = cols + [dy_template.format(d)] + ([dyw_template.format(d)] if d in with_weights else [])
     return cols
+
+
+def _get_kw(src_col, is_pt=True):
+    return '_'.join([fit.set_kw, src_col[0:2], (xcol if is_pt else wcol)])
+
+
+def _initialize_output(cols):
+    def _get_empty_fvect():
+        return np.empty((0,), dtype=float)
+    out = dict()
+    for c in cols:
+        if c[0] in [xcol, ycol, wcol]:
+            out[c] = _get_empty_fvect()
+        elif c[-1] == ycol:
+            out[_get_kw(c)] = np.empty((0, 2), dtype=float)
+        elif c[-1] == wcol:
+            out[_get_kw(c, False)] = _get_empty_fvect()
+    return out
+
+
+def _parse_line(cols, line, delim=','):
+    def _is_not_blank(s):
+        return bool(s and s.strip())
+    out = dict()
+    vals = line.split(delim)
+    x = float(vals[0])
+    for cn, rcv in zip(cols, vals):
+        cv = float(rcv) if _is_not_blank(rcv) else None
+        if cn == xcol:
+            if cv is None:
+                raise ValueError('An x value is required for every line.')
+            assert(x == cv)
+            out[xcol] = cv
+        elif cv is not None:
+            if cn in [ycol, wcol]:
+                out[cn] = cv
+            elif cn[-1] == ycol:
+                out[_get_kw(cn)] = (x, cv)
+            elif cn[-1] == wcol:
+                out[_get_kw(cn, False)] = cv
+    return out
+
+
+
+
+
+
--- examples/experimental/BSplineFitter/bfit_plotter/test/test_io.py
+++ examples/experimental/BSplineFitter/bfit_plotter/test/test_io.py
@@ -48,3 +48,93 @@ class TestIo(ut.TestCase):
     def test__get_expected_cols_p2_d0d1d2_weighted(self):
         ecols = ['x', 'y', 'w', 'd1y', 'd1w', 'd2y', 'd2w']
         nptest.assert_array_equal(ecols, io._get_expected_cols(2, [0, 1, 2]))
+
+    def test__initialize_output_xy_only(self):
+        cols = ['x', 'y']
+        out_dict = io._initialize_output(cols)
+        self.assertEqual(2, len(out_dict.keys()))
+        self.assertTrue('x' in out_dict)
+        self.assertEqual(0, out_dict['x'].size)
+        self.assertEqual((0,), out_dict['x'].shape)
+        self.assertTrue('y' in out_dict)
+        self.assertEqual(0, out_dict['y'].size)
+        self.assertEqual((0,), out_dict['y'].shape)
+
+    def test__initialize_output_xyw(self):
+        cols = ['x', 'y', 'w']
+        out_dict = io._initialize_output(cols)
+        self.assertEqual(3, len(out_dict.keys()))
+        self.assertTrue('x' in out_dict)
+        self.assertTrue('y' in out_dict)
+        self.assertTrue('w' in out_dict)
+        self.assertEqual(0, out_dict['w'].size)
+        self.assertEqual((0,), out_dict['w'].shape)
+
+    def test__initialize_output_xy_d1_no_wt(self):
+        cols = ['x', 'y', 'd1y']
+        out_dict = io._initialize_output(cols)
+        self.assertEqual(3, len(out_dict.keys()))
+        self.assertTrue('x' in out_dict)
+        self.assertTrue('y' in out_dict)
+        self.assertTrue('set_d1_x' in out_dict)
+        self.assertEqual(0, out_dict['set_d1_x'].size)
+        self.assertEqual((0,2), out_dict['set_d1_x'].shape)
+
+    def test__initialize_output_xyw_d2_weighted(self):
+        cols = ['x', 'y', 'w', 'd2y', 'd2w']
+        out_dict = io._initialize_output(cols)
+        self.assertEqual(5, len(out_dict.keys()))
+        self.assertTrue('x' in out_dict)
+        self.assertTrue('y' in out_dict)
+        self.assertTrue('y' in out_dict)
+        self.assertTrue('set_d2_x' in out_dict)
+        self.assertTrue('set_d2_w' in out_dict)
+        self.assertEqual(0, out_dict['set_d2_w'].size)
+        self.assertEqual((0,), out_dict['set_d2_w'].shape)
+
+    def test__parse_line_xy(self):
+        cols = ['x', 'y']
+        line = '3.4, 5.6'
+        out = io._parse_line(cols, line)
+        self.assertEqual(2, len(out.keys()))
+        self.assertEqual(3.4, out['x'])
+        self.assertEqual(5.6, out['y'])
+
+    def test__parse_line_xyw(self):
+        cols = ['x', 'y', 'w']
+        line = '3.4, 5.6, 2'
+        out = io._parse_line(cols, line)
+        self.assertEqual(3, len(out.keys()))
+        self.assertEqual(3.4, out['x'])
+        self.assertEqual(5.6, out['y'])
+        self.assertEqual(2.0, out['w'])
+
+    def test__parse_line_xy_d1unweighted(self):
+        cols = ['x', 'y', 'd1y']
+        line = '3.4, 5.6, 2'
+        out = io._parse_line(cols, line)
+        self.assertEqual(3, len(out.keys()))
+        self.assertEqual(3.4, out['x'])
+        self.assertEqual(5.6, out['y'])
+        self.assertEqual((3.4, 2.0), out['set_d1_x'])
+
+    def test__parse_line_xyw_d2unweighted(self):
+        cols = ['x', 'y', 'w', 'd2y']
+        line = '3.4, 5.6, 1.5, 0'
+        out = io._parse_line(cols, line)
+        self.assertEqual(4, len(out.keys()))
+        self.assertEqual(3.4, out['x'])
+        self.assertEqual(5.6, out['y'])
+        self.assertEqual(1.5, out['w'])
+        self.assertEqual((3.4, 0.0), out['set_d2_x'])
+
+    def test__parse_line_xy_d1_d2w(self):
+        cols = ['x', 'y', 'd1y', 'd2y', 'd2w']
+        line = '3.4, 5.6, 0, 1.5, 2'
+        out = io._parse_line(cols, line)
+        self.assertEqual(5, len(out.keys()))
+        self.assertEqual(3.4, out['x'])
+        self.assertEqual(5.6, out['y'])
+        self.assertEqual((3.4, 0.0), out['set_d1_x'])
+        self.assertEqual((3.4, 1.5), out['set_d2_x'])
+        self.assertEqual(2.0, out['set_d2_w'])
